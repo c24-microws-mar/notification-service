7e0f50cd585048a88ecb2d8694d70b34840537a4
Thomas Hentschel <thomas.hentschel@mail.de>
2016-03-18 14:13:35 +0100

    Send mail to static address ina synchronous way

--- order-notification/index.js
+++ order-notification/index.js
@@ -1,11 +1,38 @@
 'use strict';
 
+const sendmail = require('sendmail')();
+
 function registerRoutes(app) {
-  app.post('/order-notification', (req, res) => {
-    res.status(501).send({
-      success: false,
-      message: 'method is not yet implemented'
+  app.post('/order-notification', (req, res, next) => {
+
+    triggerSendmail({
+      from: 'c24_microserviceworkshop@hentschel.email',
+      to: 'thomas.hentschel@check24.de',
+      subject: 'test sendmail',
+      content: 'Mail of test sendmail'
+    }, function (isError) {
+      if (isError) {
+        res.status(500).send({
+          success: false,
+          message: 'an error occured while sending email notification'
+        });
+      } else {
+        res.status(200).send({
+          success: true
+        });
+      }
     });
+  });
+}
+
+function triggerSendmail(options, callback) {
+  sendmail(options, function(err, reply) {
+    console.log('answer from sendmail: ', reply);
+    if (err) {
+      console.err('cannot send mail: ', err, options);
+    }
+    const isError = !!err;
+    callback(isError);
   });
 }
 
